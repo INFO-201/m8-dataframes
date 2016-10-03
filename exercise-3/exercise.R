4785d4f4b526e5e4d0d64576f215fa0947d9fee0
Michael Freeman <michael.k.freeman@gmail.com>
2016-10-03 15:07:28 -0700

    Updated exercise-3

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -1,41 +1,55 @@
-# Exercise 3: Data Frame Calculations
-
-
 # Create a vector of 100 employees ("Employee 1", "Employee 2", ... "Employee 100)
 
 
 # Create a vector of 2014 salaries using the runif function
 
 
-# Create a vector of 2015 salaries that are typically higher than the 2014 salaires, but not always
+# Create a vector of 2015 salaries that are typically higher than the 2014 salaires (use runif again)
 
 
-# Create a data.frame `salaries` by combining the vectors you just made
+# Create a data.frame 'salaries' by combining the vectors you just made
 
 
-# In `salaries`, change the name of Employee 1 to your name. Did you get an error?
+# Create a column 'raise' that stores the size of the raise between 2014 and 2015
 
 
+# Create a column 'got.raise' that is TRUE if the person got a raise
 
-# Create a column `got.raise` that is TRUE if the person got a raise
+
+# Retrieve values from your data frame to answer the following questions:
+
+# What was the 2015 salary of employee 57
 
 
 # How many employees got a raise?
 
 
+# What was the value of the highest raise?
+
+
+# What was the name of the employee who recieved the highest raise?
+
+
+# What was the largest decrease in salaries between the two years?
+
+
+# What was the name of the employee who recieved largest decrease in salary?
+
+
 # What was the average salary increase?
-
-
-# Write a .csv file of your salaries to your working directory
 
 
 ### Bonus ###
 
+# Write a .csv file of your salaries to your working directory
+
+
 # For people who did not get a raise, how much money did they lose?
+
 
 # Is that what you expected them to lose?
 
-### Double Bonus (answers not provided) ###
+### Double Bonus (no answer given) ###
 
 # Repeat the above experiment 100 times, tracking the loss each time.
 # Does the average loss equal what you expect?
